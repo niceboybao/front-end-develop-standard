fcb7edda6b519753213dc57332396b4569a12612
baldwin <niceboybao@gmail.com>
2019-01-08 10:31:52 +0800

    ［需求／BUG］: vuepress 工程初始化 ［模块］: 无 ［说明］: 无

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,93 @@
+module.exports = {
+	title: '前端开发规范手册',
+	description: '前端开发规范手册',
+	base: '/wiki/front-end-develop-standard/', // 将部署站点的基本URL
+	head: [
+		[
+			'link',
+			{
+				rel: 'icon',
+				href: 'https://niceboybao.com/static_res/images/favicon.ico'
+			}
+		]
+	],
+	DEST: '.vuepress/dist', // 指定输出目录
+	themeConfig: {
+		lastUpdated: 'Last Updated', // 最后更新时间
+		displayAllHeaders: true, // 展开所有标题
+		nav: [
+			{
+				text: '快速入门',
+				link: '/'
+			},
+			{
+				text: '工程规范',
+				link: '/html'
+			},
+			{
+				text: '代码规范',
+				items: [
+					{
+						text: 'html',
+						link: '/language/chinese'
+					},
+					{
+						text: 'css',
+						link: '/language/japanese'
+					},
+					{
+						text: 'javascript',
+						link: '/language/japanese'
+					}
+				]
+			},
+			{
+				text: '关于',
+				link: '/about/'
+			}
+		],
+		sidebar: {
+			// 关于
+			'/about/': [
+				{
+					title: '关于',
+					collapsable: false,
+					children: [ '/about/' ]
+				}
+			],
+			// 首页
+			'/': [
+				{
+					title: '快速入门 1',
+					collapsable: false,
+					children: [ '/' ]
+				},
+				{
+					title: '快速入门 2',
+					collapsable: false,
+					children: [ '/page-a' ]
+				}
+			]
+		}
+		// sidebar: {
+		//     '/foo/': [
+		//       '',     /* /foo/ */
+		//       'one',  /* /foo/one.html */
+		//       'two'   /* /foo/two.html */
+		//     ],
+
+		//     '/bar/': [
+		//       '',      /* /bar/ */
+		//       'three', /* /bar/three.html */
+		//       'four'   /* /bar/four.html */
+		//     ],
+
+		//     // fallback
+		//     '/': [
+		//       '',        /* / */
+		//       'contact', /* /contact.html */
+		//       'about'    /* /about.html */
+		//     ]
+		//   }
+	}
+};
---
+++ docs/about/README.md
@@ -0,0 +1,3 @@
+# 关于
+
+在 [Build 2015](https://www.iplaysoft.com/build-2015.html) 大会上，微软除了发布了 Microsoft Edge 浏览器和新的 Windows 10 系统外，最大的惊喜莫过于宣布推出免费跨平台的 Visual Studio Code 编辑器了！
