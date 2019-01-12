99d95c3723809c0b0a4400995e69b2ef73e7b41c
baldwin <niceboybao@gmail.com>
2019-01-12 17:08:19 +0800

    ［需求／BUG］: 新增项目应用规范 ［模块］: 无 ［说明］: 无

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,115 +2,141 @@
  * @Author: guangwei.bao
  * @Date: 2019-01-08 14:39:31
  * @Last Modified by: guangwei.bao
- * @Last Modified time: 2019-01-10 15:31:45
+ * @Last Modified time: 2019-01-12 15:19:19
  * @Describe: vuepress 配置文件
  */
 module.exports = {
-    title: '前端开发规范手册',
-    description: '前端开发规范手册',
-    base: '/wiki/front-end-develop-standard/', // 将部署站点的基本URL
-    head: [
-        [
-            'link',
-            {
-                rel: 'icon',
-                href: 'https://niceboybao.com/static_res/images/favicon.ico'
-            }
-        ]
-    ],
-    DEST: '.vuepress/dist', // 指定输出目录
-    themeConfig: {
-        lastUpdated: '上次更新',
-        serviceWorker: {
-            updatePopup: {
-                message: '发现新内容可用',
-                buttonText: '刷新'
-            }
-        },
-        displayAllHeaders: true, // 展开所有标题
-        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'https://github.com/niceboybao/front-end-develop-standard',
-        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-        repoLabel: '查看源码',
+	title: '前端开发规范手册',
+	description: '前端开发规范手册',
+	base: '/wiki/front-end-develop-standard/', // 将部署站点的基本URL
+	head: [
+		[
+			'link',
+			{
+				rel: 'icon',
+				href: 'https://niceboybao.com/static_res/images/favicon.ico'
+			}
+		]
+	],
+	DEST: '.vuepress/dist', // 指定输出目录
+	themeConfig: {
+		lastUpdated: '上次更新',
+		serviceWorker: {
+			updatePopup: {
+				message: '发现新内容可用',
+				buttonText: '刷新'
+			}
+		},
+		displayAllHeaders: true, // 展开所有标题
+		// 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+		repo: 'https://github.com/niceboybao/front-end-develop-standard',
+		// 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+		// "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+		repoLabel: '查看源码',
 
-        // 以下为可选的编辑链接选项
-        // 假如你的文档仓库和项目本身不在一个仓库：
-        // docsRepo: 'vuejs/vuepress',
-        // 假如文档不是放在仓库的根目录下：
-        docsDir: 'docs',
-        // 假如文档放在一个特定的分支下：
-        docsBranch: 'master',
-        // 默认是 false, 设置为 true 来启用
-        editLinks: true,
-        // 默认为 "Edit this page"
-        editLinkText: '在GitHub上编辑此页面',
-        nav: [{
-                text: '快速入门',
-                link: '/'
-            },
-            {
-                text: '命名规范',
-                link: '/name/'
-            },
-            {
-                text: '代码规范',
-                items: [{
-                        text: 'HTML',
-                        link: '/code/html/'
-                    },
-                    {
-                        text: 'CSS',
-                        link: '/code/css/'
-                    },
-                    {
-                        text: 'JavaScript',
-                        link: '/code/javascript/'
-                    }
-                ]
-            },
-            {
-                text: '关于',
-                link: '/about/'
-            }
-        ],
-        sidebar: {
-            // 命名规范
-            '/name/': [{
-                title: '',
-                collapsable: false,
-                children: ['/name/']
-            }],
-            // 代码规范 html
-            '/code/html/': [{
-                title: '',
-                collapsable: false,
-                children: ['/code/html/']
-            }],
-            // 代码规范 css
-            '/code/css/': [{
-                title: '',
-                collapsable: false,
-                children: ['/code/css/']
-            }],
-            // 代码规范 javascript
-            '/code/javascript/': [{
-                title: '',
-                collapsable: false,
-                children: ['/code/javascript/']
-            }],
-            // 关于
-            '/about/': [{
-                title: '',
-                collapsable: false,
-                children: ['/about/']
-            }],
-            // 快速入门
-            '/': [{
-                title: '',
-                collapsable: false,
-                children: ['/']
-            }]
-        }
-    }
+		// 以下为可选的编辑链接选项
+		// 假如你的文档仓库和项目本身不在一个仓库：
+		// docsRepo: 'vuejs/vuepress',
+		// 假如文档不是放在仓库的根目录下：
+		docsDir: 'docs',
+		// 假如文档放在一个特定的分支下：
+		docsBranch: 'master',
+		// 默认是 false, 设置为 true 来启用
+		editLinks: true,
+		// 默认为 "Edit this page"
+		editLinkText: '在GitHub上编辑此页面',
+		nav: [
+			{
+				text: '快速入门',
+				link: '/'
+			},
+			{
+				text: '命名规范',
+				link: '/name/'
+			},
+			{
+				text: '代码规范',
+				items: [
+					{
+						text: 'HTML',
+						link: '/code/html/'
+					},
+					{
+						text: 'CSS',
+						link: '/code/css/'
+					},
+					{
+						text: 'JavaScript',
+						link: '/code/javascript/'
+					}
+				]
+			},
+			{
+				text: '项目应用',
+				link: '/use/'
+			},
+			{
+				text: '关于',
+				link: '/about/'
+			}
+		],
+		sidebar: {
+			// 命名规范
+			'/name/': [
+				{
+					title: '',
+					collapsable: false,
+					children: [ '/name/' ]
+				}
+			],
+			// 代码规范 html
+			'/code/html/': [
+				{
+					title: '',
+					collapsable: false,
+					children: [ '/code/html/' ]
+				}
+			],
+			// 代码规范 css
+			'/code/css/': [
+				{
+					title: '',
+					collapsable: false,
+					children: [ '/code/css/' ]
+				}
+			],
+			// 代码规范 javascript
+			'/code/javascript/': [
+				{
+					title: '',
+					collapsable: false,
+					children: [ '/code/javascript/' ]
+				}
+			],
+			// 关于
+			'/about/': [
+				{
+					title: '',
+					collapsable: false,
+					children: [ '/about/' ]
+				}
+			],
+			// 项目应用
+			'/use/': [
+				{
+					title: '',
+					collapsable: false,
+					children: [ '/use/' ]
+				}
+			],
+			// 快速入门
+			'/': [
+				{
+					title: '',
+					collapsable: false,
+					children: [ '/' ]
+				}
+			]
+		}
+	}
 };
--- docs/about/README.md
+++ docs/about/README.md
@@ -1,5 +1,5 @@
 ---
-prev: ../code/javascript/
+prev: ../use/
 next: false
 ---
 
@@ -8,6 +8,24 @@ next: false
 工程架构基于[vuepress](https://v0.vuepress.vuejs.org/)生成，源码地址[github](https://github.com/niceboybao/front-end-develop-standard)欢迎指点
 
 文档编写参考[airbnb JavaScript样式指南](https://github.com/airbnb/javascript)，[github代码库总结出来的工程师代码书写习惯](http://alloyteam.github.io/CodeGuide/#js-indentation)等多个热门的前端开源规范库,并结合个人项目总结而成。
+
+## 关于手册
+
+本手册版权由<https://niceboybao.com/>域名下所有，转载请注明出处，联系作者`niceboybao@163.com`
+
+## 关于作者
+
+[我的GitHub](https://github.com/niceboybao)、[我的掘金](https://juejin.im/user/58ec89745c497d0062c633f7/posts)、[我的博客作品](https://niceboybao.com/) 欢迎有志同道合的小伙伴一起交流
+
+如需联系作者->`niceboybao@163.com`。
+
+或微信号请扫码 **备注：wiki+昵称**
+
+![wechat.png](../images/wechat01.png)
+
+**或关注如下前端公众号一起进步**
+
+![wechat.png](../images/wechat02.png)
 
 ## 参考文档
 
@@ -22,7 +40,7 @@ next: false
 - [AMD 和 CMD 的区别有哪些？](https://www.zhihu.com/question/20351507)
 - [AMD规范与CMD规范的区别](http://www.cnblogs.com/tugenhua0707/p/3507957.html)
 - [网易前端开发规范](http://nec.netease.com/standard)
-- [React开发规范](https://github.com/ecomfe/spec/blob/master/react-style-guide.md)
+- [前端开发规范大全(包括react.js规范)](https://github.com/ecomfe/spec/blob/master/react-style-guide.md)
 
 ## 开源协议
 
